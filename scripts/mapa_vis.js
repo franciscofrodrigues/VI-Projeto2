2d07b89cbc1cb4e6fbdca81a11ac6a8ca8001dab
f_rodrigues <franciscofrodrigues28@gmail.com>
2023-12-09 19:40:23 +0000

    Mapa

--- scripts/mapa_vis.js
+++ scripts/mapa_vis.js
@@ -1,12 +1,14 @@
-// the data 
+// The data 
 const geoMap = "https://gisco-services.ec.europa.eu/distribution/v2/nuts/geojson/NUTS_RG_60M_2013_4326_LEVL_3.geojson";
 const natMort = "data/NatalidadeMortalidade.csv";
 const demographics = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv";
 
+
 // The svg
-let svg = d3.select("#my_tree"),
+let svg = d3.select("#mapa"),
 width = +svg.attr("width"),
 height = +svg.attr("height");
+
 
 // Map and projection
 // info: https://github.com/d3/d3-geo/blob/v3.0.1/README.md#geoPath
@@ -18,11 +20,13 @@ let projection = d3.geoMercator()
 .center([-8, 39.5])   
 .translate([width / 2, height / 2]);
 
+
 const customColors = ['#12121A', '#3E3F59', '#6A6C99', '#9699D9'];
 
 let colorScale = d3.scaleThreshold()
-.domain([5, 7, 9, 11])
+.domain([8, 12, 15, 18.7])
 .range(customColors);
+
 
 // Load dados externos para o array
 let promises = [
@@ -31,36 +35,73 @@ let promises = [
         return {
             nome: d.nome,
             codigo: "PT" + d.codigo,
-            tbn: +d.tbn.replace(" ", "").replace(",", ".")
+            tbn: +d.tbn.replace(" ", "").replace(",", "."),
+            tbm: +d.tbm.replace(" ", "").replace(",", ".")
         };
     })
     ];
 
 Promise.all(promises).then(draw_map);
 
-function draw_map(data) {
-    console.log(data[1]);
 
-    // Desenha o mapa
-    svg.append("g")
-    .selectAll("path")
-        .data(data[0]['features']) // mapa com as shapes
+function createPattern(defs, line) {
+  const pattern = defs.append("pattern")
+  .attr("id", "pattern_" + line.codigo)
+  .attr("patternUnits", "userSpaceOnUse")
+  .attr("width", line.tbn)
+  .attr("height", line.tbn);
+
+  pattern.append("circle")
+  .attr("cx", line.tbn / 2)
+  .attr("cy", line.tbn / 2)
+  .attr("r", function() {
+      return d3.scaleThreshold()
+      .domain([6, 7, 8, 9])
+      .range([line.tbn / 8, line.tbn / 6, line.tbn / 4, line.tbn / 3])(line.tbn);
+  })
+  .attr("fill", "#f2f2f2");
+}
+
+
+function draw_map(data) {
+
+  const defs = svg.append("defs");
+
+  data[1].forEach(function (line) {
+    createPattern(defs, line); // criar padrão com base nos dados de TBN
+  });
+
+  let region = svg.append("g");
+
+  region.selectAll("path") // desenha o mapa
+        .data(data[0]['features']) // mapa com as formas
         .enter()
         .filter(function (d){
-            // desenhar apenas os países com códigos que começam com "PT"
-            return d.properties.NUTS_ID.startsWith("PT");
+            return d.properties.NUTS_ID.startsWith("PT"); // desenhar apenas os países com códigos que começam com "PT"
         })
         .append("path")
-        // desenha cada país consoante a projecção definida
-        .attr("d", d3.geoPath().projection(projection))
-        // define a cor consoante a população
-        .attr("fill", function (d) {
-            // ir buscar a linha correspondente ao país
-            let line = data[1].find(o => o.codigo === d.properties.NUTS_ID)
-            console.log('Line:', line);
-
+        .attr("d", d3.geoPath().projection(projection)) // desenha cada país consoante a projecção definida
+        .attr("fill", function (d) { // define a cor consoante a TBM
+            let line = data[1].find(o => o.codigo === d.properties.NUTS_ID) // ir buscar a linha correspondente ao país
             if (line) {
-                return colorScale(line['tbn']);
-            }
-});
-}
+                return colorScale(line['tbm']);
+            }            
+        })
+        .attr("stroke", "#F2F2F2")
+        .attr("stroke-width", 1);
+
+  region.selectAll("path")
+        .data(data[0]['features']) // Map with the shapes
+        .enter()
+        .filter(function (d) {
+            return d.properties.NUTS_ID.startsWith("PT"); // Draw only the countries with codes starting with "PT"
+        })
+        .append("path")
+        .attr("d", d3.geoPath().projection(projection))
+        .attr("fill", function (d) {
+          let line = data[1].find(o => o.codigo === d.properties.NUTS_ID);
+          if (line) {
+            return "url(#pattern_" + line.codigo + ")";
+        }
+    });
+    }
