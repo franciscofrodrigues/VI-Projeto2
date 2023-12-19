3d5ba5191cae85d34574957e89e1213632039c10
Ana2020143716 <anacsrodrigues.46@gmail.com>
2023-12-19 00:07:42 +0000

    star_plotV6.2

---
+++ scripts/star_plot2.js
@@ -0,0 +1,132 @@
+var csvData;
+
+// Função para atualizar o gráfico de radar com base na seleção do usuário
+function updateRadarChart() {
+    var selectedNome = document.getElementById('nomeSelector').value;
+
+    // Encontra os dados correspondentes ao país selecionado
+    var selectedData = csvData.find(function (d) {
+        return d.nome === selectedNome;
+    });
+
+    // Remove o gráfico anterior se existir
+    d3.select('svg').remove();
+
+    // Configuração do radar chart
+    var margin = { top: 50, right: 50, bottom: 50, left: 50 },
+        width = 400 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    // Configuração do SVG
+    var svg = d3.select('body').append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr('transform', 'translate(' + (width / 2 + margin.left) + ',' + (height / 2 + margin.top) + ')');
+
+    // Número de variáveis (número de lados no radar chart)
+    var numVars = 5;  // número de faixas etárias no seu exemplo
+
+    // Angulo de cada eixo no radar chart
+    var angleSlice = Math.PI * 2 / numVars;
+
+    // Configuração da escala radial
+    var rScale = d3.scaleLinear()
+        .range([0, Math.min(width / 2, height / 2)]);
+
+    // Linhas do radar chart
+    var radarLine = d3.lineRadial()
+        .curve(d3.curveLinearClosed)
+        .radius(function (d) { return rScale(d); })
+        .angle(function (d, i) { return i * angleSlice; });
+
+    // Adiciona os eixos
+  var eixos = svg.selectAll('.axis')
+  .data(d3.range(numVars))
+  .enter()
+  .append('g')
+  .attr('class', 'axis')
+  .style('stroke', 'black');
+
+    // Adiciona as linhas dos eixos
+  eixos.append('line')
+  .attr('x1', 0)
+  .attr('y1', 0)
+  .attr('x2', function (d, i) { return rScale.range()[1] * Math.cos(angleSlice * i - Math.PI / 2); })
+  .attr('y2', function (d, i) { return rScale.range()[1] * Math.sin(angleSlice * i - Math.PI / 2); })
+  .attr('class', 'axisLine')
+  .style('stroke', 'black');
+
+  
+    // Lê os dados do CSV e cria o radar chart
+    var faixasEtarias = Object.keys(selectedData).filter(key => key !== "codigo" && key !== "nome");
+    var valores = faixasEtarias.map(key => +selectedData[key]); // Converte os valores para números
+
+    // Atualiza a escala radial com base nos novos valores
+    rScale.domain([0, d3.max(valores)]);
+
+    // Adiciona os marcadores (pontos) nos eixos
+  eixos.append('circle')
+  .attr('class', 'axisMarker')
+  .attr('r', 5)  // raio do marcador
+  .attr('cx', function (d, i) { return rScale(valores[i]) * Math.cos(angleSlice * i - Math.PI / 2); })
+  .attr('cy', function (d, i) { return rScale(valores[i]) * Math.sin(angleSlice * i - Math.PI / 2); })
+  .on('mouseover', function (d, i) {
+    showTooltip(d, i); // Função para exibir tooltip
+  })
+  .on('mouseout', function () {
+    hideTooltip(); // Função para ocultar tooltip
+  });;
+
+// Função para exibir o tooltip
+function showTooltip(d, i) {
+    var tooltip = d3.select('#tooltip');
+    
+    // Use d3.event.pageX e d3.event.pageY diretamente se disponíveis, caso contrário, use event.clientX e event.clientY
+    var xPosition = d3.event ? d3.event.pageX : event.clientX + window.pageXOffset;
+    var yPosition = d3.event ? d3.event.pageY : event.clientY + window.pageYOffset;
+  
+    tooltip.html(valores[i])
+      .style('left', (xPosition + 10) + 'px')
+      .style('top', (yPosition - 10) + 'px')
+      .style('display', 'block');
+  }
+  
+  // Função para ocultar o tooltip
+  function hideTooltip() {
+    d3.select('#tooltip').style('display', 'none');
+  }
+
+
+// Adiciona as legendas nos eixos
+eixos.append('text')
+.attr('class', 'axisLabel')
+.attr('x', function (d, i) { return rScale.range()[1] * Math.cos(angleSlice * i - Math.PI / 2); })
+.attr('y', function (d, i) { return rScale.range()[1] * Math.sin(angleSlice * i - Math.PI / 2); })
+.attr('dy', -10)  // ajuste de posição vertical
+.text(function (d, i) { return faixasEtarias[i]; });
+
+
+    // Adiciona os paths
+    svg.append('path')
+        .attr('class', 'radarLine')
+        .datum(valores)
+        .attr('d', radarLine)
+        .style('stroke', 'black')
+        .style('fill', 'blue')
+        .style('fill-opacity', 0.2);
+}
+
+// Lê o arquivo CSV para obter a lista de países
+d3.csv('./data/DatasetResidentes2.csv').then(function (data) {
+    csvData = data;  // Armazena os dados globalmente
+
+    // Adiciona as opções à caixa de seleção
+    var selector = document.getElementById('nomeSelector');
+    data.forEach(function (d) {
+        var option = document.createElement('option');
+        option.value = d.nome;
+        option.text = d.nome;
+        selector.add(option);
+    });
+});
