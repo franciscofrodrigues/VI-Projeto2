39628a124ae514cd07ec16aa83421db3db2dc4c4
f_rodrigues <franciscofrodrigues28@gmail.com>
2023-12-19 21:32:44 +0000

    Interação Mapa

--- scripts/mapa_vis.js
+++ scripts/mapa_vis.js
@@ -8,27 +8,23 @@ width = +svg.attr("width"),
 height = +svg.attr("height");
 
 // Mapa e projeção
-// info: https://github.com/d3/d3-geo/blob/v3.0.1/README.md#geoPath
 let path = d3.geoPath();
 
-// info: https://github.com/d3/d3-geo#projections
 let projection = d3.geoMercator()
 .scale(7000)
-.center([-8, 39.5])   
+.center([-8, 39.5])
 .translate([width / 2, height / 2]);
-
 
 const customColors = ['#12121A', '#3E3F59', '#6A6C99', '#9699D9'];
 
 let colorScale = d3.scaleThreshold()
-.domain([8, 12, 15, 18.7]) // cores das regiões com base nos limites definidos
+.domain([8, 12, 15, 18.7])
 .range(customColors);
-
 
 // Carregar dados externos para o array
 let promises = [
   d3.json(geoMap),
-  d3.csv(natMort, function(d) { 
+  d3.csv(natMort, function(d) {
     return {
       nome: d.nome,
       codigo: "PT" + d.codigo,
@@ -36,10 +32,9 @@ let promises = [
       tbm: +d.tbm
     };
   })
-];
+  ];
 
 Promise.all(promises).then(draw_map);
-
 
 function createPattern(defs, line) {
   const pattern = defs.append("pattern")
@@ -47,60 +42,86 @@ function createPattern(defs, line) {
   .attr("patternUnits", "userSpaceOnUse")
   .attr("width", line.tbn)
   .attr("height", line.tbn);
-  
+
   pattern.append("circle")
   .attr("cx", line.tbn / 2)
   .attr("cy", line.tbn / 2)
   .attr("r", function() {
     let radiusScale = d3.scaleThreshold()
     .domain([6, 7, 8, 9])
-    .range([0.5, 1.5, 2.5, 3.5]); // raio dos circulos com base nos limites definidos
+    .range([0.5, 1.5, 2.5, 3.5]);
     return radiusScale(line.tbn);
   })
-  .attr("fill", "#f2f2f2");
+  .attr("fill", "#F2F2F2");
 }
 
 
 function draw_map(data) {
-  
-  const defs = svg.append("defs"); // criação dos padrões
-  
-  data[1].forEach(function (line) {
-    createPattern(defs, line); // criar padrão com base nos dados de TBN
+  const defs = svg.append("defs"); // Create pattern definitions
+
+  data[1].forEach(function(line) {
+    createPattern(defs, line); // Create pattern with TBN data
   });
-  
-  let region = svg.append("g");
-  
-  region.selectAll("path") // desenha o mapa
-  .data(data[0]['features']) // mapa com as formas
-  .enter()
-  .filter(function (d){
-    return d.properties.NUTS_ID.startsWith("PT"); // desenhar apenas os países com códigos que começam com "PT"
+
+  const region = {};
+
+  data[0]['features']
+  .filter(function(d) {
+    return d.properties.NUTS_ID.startsWith("PT");
   })
-  .append("path")
-  .attr("d", d3.geoPath().projection(projection)) // desenha cada país consoante a projecção definida
-  .attr("fill", function (d) { // define a cor consoante a TBM
-    let line = data[1].find(o => o.codigo === d.properties.NUTS_ID) // ir buscar a linha correspondente ao país
-    if (line) {
-      return colorScale(line['tbm']);
-    }            
-  })
-  .attr("stroke", "#F2F2F2")
-  .attr("stroke-width", 1);
-  
-  
-  region.selectAll("path")
-  .data(data[0]['features']) // mapa com as formas
-  .enter()
-  .filter(function (d) {
-    return d.properties.NUTS_ID.startsWith("PT"); // desenhar apenas os países com códigos que começam com "PT"
-  })
-  .append("path")
-  .attr("d", d3.geoPath().projection(projection))
-  .attr("fill", function (d) {
-    let line = data[1].find(o => o.codigo === d.properties.NUTS_ID);
-    if (line) {
-      return "url(#pattern_" + line.codigo + ")";
+  .forEach(function(d) {
+    const codigo = d.properties.NUTS_ID;
+
+    if (!region[codigo]) {
+      region[codigo] = svg.append("g").attr("class", codigo);
     }
+
+    const regionGroup = region[codigo];
+
+    regionGroup.append("path")
+    .attr("class", "region")
+    .attr("d", path.projection(projection)(d))
+    .attr("fill", function() {
+      const line = data[1].find(o => o.codigo === codigo);
+      if (line) {
+        return colorScale(line['tbm']);
+      }            
+    })
+    .attr("stroke", "#F2F2F2")
+    .attr("stroke-width", 1);
+
+    regionGroup.append("path")
+    .attr("class", "pattern")
+    .attr("d", path.projection(projection)(d))
+    .attr("fill", function() {
+      const line = data[1].find(o => o.codigo === codigo);
+      if (line) {
+        return "url(#pattern_" + line.codigo + ")";
+      }
+    })
+    .on('mouseover', function() {
+      const line = data[1].find(o => o.codigo === codigo);
+      let tbnValue, tbmValue, regionName = '';
+      if (line) {
+        regionName = line.nome;
+        tbnValue = line.tbn;
+        tbmValue = line.tbm;
+      }
+
+
+      d3.select("#tbnH4").text('Taxa Bruta de Natalidade – '+tbnValue);
+      d3.select("#tbmH4").text('Taxa Bruta de Mortalidade – '+tbmValue);
+      d3.select("#regionSelected").text(regionName);
+
+      svg.selectAll(".region").attr("opacity", 0.4);
+      d3.select(this.parentNode).select(".region").attr("opacity", 1);
+    })
+    .on('mouseout', function() {
+      d3.select("#tbnH4").text('');
+      d3.select("#tbmH4").text('');
+      d3.select("#regionSelected").text('');
+
+      svg.selectAll(".region").attr("opacity", 1);
+    });
   });
 }
